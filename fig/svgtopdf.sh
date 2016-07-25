c19d4b5bce23ba5233155178e1f63fba8b0a3c0f
edisz <eduardszoecs@gmail.com>
2016-07-25 14:29:24 +0200

    new gitignore

---
+++ fig/svgtopdf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for var in "$@"
+do
+	echo '*****'
+	s=${var##*/}
+	base=${s%.svg}
+	echo 'converting' $var
+
+	LD_LIBRARY_PATH=$HOME/cairo-git/lib inkscape -f $var -A $base.pdf
+	echo 'done!'
+done
